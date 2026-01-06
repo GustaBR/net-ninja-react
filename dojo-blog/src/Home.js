5f7e40d13db85588ea4c68ccc56f589c8812e209
Gustavo Henrique <gustavo.tin2@gmail.com>
2026-01-06 19:27:46 -0300

    lesson 7 completed

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("Hello, Ninjas!");
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("Hello,", name);
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain("Mario")}>Click me again</button>
         </div>
     );
 }
