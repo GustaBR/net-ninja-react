551299058564e7c2adce94748ef46fc711a8de2f
Gustavo Henrique <gustavo.tin2@gmail.com>
2026-02-21 14:48:55 -0300

    lesson 20 completed

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,33 +1,9 @@
 import { useState, useEffect } from 'react';
+import useFetch from './useFetch';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-      .then(res => {
-        if (!res.ok) {
-          throw Error("Could not fetch the data");
-        }
-          
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsLoading(false);
-      });
-    }, 1000);
-    
-  });
+  const { data: blogs, isLoading, error } = useFetch("http://localhost:8000/blogs"); 
 
   return (
     <div className="home">
