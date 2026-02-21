e7e29442561062dfe325c0c317936b09bc7f302c
Gustavo Henrique <gustavo.tin2@gmail.com>
2026-02-21 15:11:18 -0300

    lessons 21-22 completed

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
