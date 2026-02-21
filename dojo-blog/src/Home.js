5d9bfaa2844786aa29f8203ffd3299640076e1c0
Gustavo Henrique <gustavo.tin2@gmail.com>
2026-02-21 13:12:17 -0300

    lessons 18-19 completed

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,17 +3,36 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-    .then(res => res.json())
-    .then(data => {
-      setBlogs(data);
-    });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+      .then(res => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data");
+        }
+          
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+    }, 1000);
+    
   });
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
